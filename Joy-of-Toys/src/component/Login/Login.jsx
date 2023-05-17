c59cd59b36e6a846608c857fdc862d7c4fb3e9b7
Shamiul haque <shamiulhaque1218@gmail.com>
2023-05-17 22:11:35 +0600

    first commit

---
+++ Joy-of-Toys/src/component/Login/Login.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            This is login
+        </div>
+    );
+};
+
+export default Login;
---
+++ Joy-of-Toys/src/main.jsx
@@ -0,0 +1,12 @@
+// https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-client-side-shamiulhaque1218
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
