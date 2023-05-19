3278e02b5288718a675eea1dfe0fa2e702b51d53
Shamiul haque <shamiulhaque1218@gmail.com>
2023-05-20 03:27:02 +0600

    galary section

--- Joy-of-Toys/src/component/Home/Home.jsx
+++ Joy-of-Toys/src/component/Home/Home.jsx
@@ -1,16 +1,38 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./Banner";
 import Product from "./Product";
-
+import Galary from "./Galary";
 
 const Home = () => {
+  const [toyData, setToyData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/toy")
+      .then((res) => res.json())
+      .then((data) => setToyData(data));
+  }, []);
   return (
     <div>
       <Banner> </Banner>
-      <Product> </Product>
+
+      <div className="pt-5 pb-10">
+      {
+        toyData.map(result => 
+          <Product key={result.id} result={result}
+           ></Product> )
+      }
+      </div>
+
+
+      <div>
+      {
+        toyData.map(result => 
+           <Galary key={result.id} result={result}
+           ></Galary> )
+      }
+      </div>
     </div>
-    
   );
 };
 
--- Joy-of-Toys/src/component/Home/Product.jsx
+++ Joy-of-Toys/src/component/Home/Product.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
-const Product = () => {
-    return (
-        
-        <div className="py-2 text-center">
-         <p className='text-3xl py-12 gFont2'> Product Categories </p>
+const Product = (props) => {
+  console.log(props.result[0].image_url);
+  return (
+    <div className="py-2 text-center">
+      <p className="text-3xl py-12 gFont2"> Product Categories </p>
       <Tabs>
         <TabList>
           <Tab>Teddy Bear</Tab>
@@ -16,17 +17,180 @@ const Product = () => {
         </TabList>
 
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className="grid grid-cols-3 px-10">
+
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img className="h-40 w-56 rounded-lg"
+                src={props.result[0].image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{props.result[0].name}</h2>
+              <p className="text-start">Price: {props.result[0].price} TK</p>
+              <p className="text-start">Rating: {props.result[0].rating}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img className="h-40 w-56 rounded-lg"
+                src={props.result[1].image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{props.result[1].name}</h2>
+              <p className="text-start">Price: {props.result[1].price} TK</p>
+              <p className="text-start">Rating: {props.result[1].rating}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img className="h-40 w-56 rounded-lg"
+                src={props.result[2].image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{props.result[2].name}</h2>
+              <p className="text-start">Price: {props.result[2].price} TK</p>
+              <p className="text-start">Rating: {props.result[2].rating}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          
+        <div className="grid grid-cols-3 px-10">
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[3].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[3].name}</h2>
+    <p className="text-start">Price: {props.result[3].price} TK</p>
+    <p className="text-start">Rating: {props.result[3].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[4].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[4].name}</h2>
+    <p className="text-start">Price: {props.result[4].price} TK</p>
+    <p className="text-start">Rating: {props.result[4].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[5].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[5].name}</h2>
+    <p className="text-start">Price: {props.result[5].price} TK</p>
+    <p className="text-start">Rating: {props.result[5].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+</div>
+          
         </TabPanel>
         <TabPanel>
-          <h2>Any content 3</h2>
+         
+        <div className="grid grid-cols-3 px-10">
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[6].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[6].name}</h2>
+    <p className="text-start">Price: {props.result[6].price} TK</p>
+    <p className="text-start">Rating: {props.result[6].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[7].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[7].name}</h2>
+    <p className="text-start">Price: {props.result[7].price} TK</p>
+    <p className="text-start">Rating: {props.result[7].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img className="h-40 w-56 rounded-lg"
+      src={props.result[8].image_url}
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{props.result[8].name}</h2>
+    <p className="text-start">Price: {props.result[8].price} TK</p>
+    <p className="text-start">Rating: {props.result[8].rating}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+</div>
+
         </TabPanel>
       </Tabs>
     </div>
-    );
+  );
 };
 
 export default Product;
